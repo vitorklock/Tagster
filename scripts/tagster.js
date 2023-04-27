826662b2f9b3243104333c4213e1d4c9a83c623a
vitorklock <klockvitor@gmail.com>
2023-04-27 18:12:25 -0300

    Minor

--- scripts/tagster.js
+++ scripts/tagster.js
@@ -21,7 +21,7 @@ class Tagster {
                 <svg class="tgs_moveLeft" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 512"><!--! Font Awesome Pro 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M9.4 278.6c-12.5-12.5-12.5-32.8 0-45.3l128-128c9.2-9.2 22.9-11.9 34.9-6.9s19.8 16.6 19.8 29.6l0 256c0 12.9-7.8 24.6-19.8 29.6s-25.7 2.2-34.9-6.9l-128-128z"/></svg>
                 <svg class="tgs_moveRight" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 512"><!--! Font Awesome Pro 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M246.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-128-128c-9.2-9.2-22.9-11.9-34.9-6.9s-19.8 16.6-19.8 29.6l0 256c0 12.9 7.8 24.6 19.8 29.6s25.7 2.2 34.9-6.9l128-128z"/></svg></div>
                 <textarea>${tag}</textarea>
-            <button onclick="$(this).parent().remove();">x</button> </span>`;
+            <button onclick="$(this).parent().remove();"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><!--! Font Awesome Pro 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z"/></svg></button> </span>`;
 
     #focusTextAreaEnd = (textarea) => {
         const $textarea = $(textarea);
@@ -57,7 +57,7 @@ class Tagster {
         let $elm = $(elm);
         const lineHeight = this.stylings.lineheight;
 
-        $elm.css('width', $elm.val().length + 2 + 'ch');
+        $elm.css('width', $elm.val().length + 3 + 'ch');
         $elm.css('height', lineHeight);
 
         if ($elm.outerWidth() >= $elm.parent().width()) {
@@ -123,6 +123,10 @@ class Tagster {
         if (!Array.isArray(tags)) tags = [tags];
 
         for (let tag of tags) {
+            console.log(this.configs.allowDuplicateTags)
+            console.log(this.tags)
+            console.log(tag)
+
             if (!this.configs.allowDuplicateTags && this.tags.includes(tag)) return;
 
             if (this.trigger('beforeAddTag', { tag: tag, })) return;
@@ -172,7 +176,7 @@ class Tagster {
     };
 
     get tags() {
-        return this.$.find('.tgs_tag').toArray().map(t => this.#clearString(t.innerText));
+        return this.$.find('.tgs_tag textarea').toArray().map(t => this.#clearString(t.val()));
     };
 
 
